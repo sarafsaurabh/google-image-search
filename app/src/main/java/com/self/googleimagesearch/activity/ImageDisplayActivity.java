6261d71b615e88ef03c4c8758e3d52dfd887689c
ssaraf <ssaraf@visa.com>
2015-09-26 00:34:46 -0700

    added sharing option

--- app/src/main/java/com/self/googleimagesearch/activity/ImageDisplayActivity.java
+++ app/src/main/java/com/self/googleimagesearch/activity/ImageDisplayActivity.java
@@ -1,33 +1,68 @@
 package com.self.googleimagesearch.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.self.googleimagesearch.R;
 import com.self.googleimagesearch.model.ImageResult;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageDisplayActivity extends AppCompatActivity {
+
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
-
-        getSupportActionBar().hide();
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         ImageResult imageResult = getIntent().getParcelableExtra("imageResult");
         ImageView ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
-        Picasso.with(this).load(imageResult.url).resize(1000, 0).into(ivImageResult);
+        Picasso.with(this).load(imageResult.url).resize(1000, 0).into(ivImageResult, new Callback() {
+            @Override
+            public void onSuccess() {
+                setupShareIntent();
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.share_fail), Toast.LENGTH_SHORT).show();
+                Log.e(getClass().toString(), "Error encountered while sharing image");
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.mi_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
         return true;
     }
 
@@ -40,4 +75,46 @@ public class ImageDisplayActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    private Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS),
+                    "image" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.share_fail), Toast.LENGTH_SHORT).show();
+            Log.e(getClass().toString(), "Error encountered while sharing image: " + e);
+        }
+        return bmpUri;
+    }
+
+    public void setupShareIntent() {
+        // Fetch Bitmap Uri locally
+        ImageView ivImage = (ImageView) findViewById(R.id.ivImageResult);
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        miShareAction.setShareIntent(shareIntent);
+    }
+
 }
